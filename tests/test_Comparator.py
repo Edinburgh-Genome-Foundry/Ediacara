5d0e21913e3039c0cb28da5c92069bd03d305724
Peter Vegh <pvegh@posteo.uk>
2021-06-06 14:01:41 +0100

    Add test file

---
+++ tests/test_Comparator.py
@@ -0,0 +1,17 @@
+import ediacara as edi
+
+
+def test_comparator():
+    edi.Comparator
+
+
+def test_comparatorgroup():
+    edi.ComparatorGroup
+
+
+def test_write_pdf_report():
+    edi.write_pdf_report
+
+
+def write_comparatorgroup_report():
+    edi.write_comparatorgroup_report
